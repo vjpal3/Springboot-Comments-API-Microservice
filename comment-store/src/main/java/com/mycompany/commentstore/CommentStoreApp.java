d1850775d5918b5011937bdec1d7da9d44d69410
vjpal3 <vrishalipal@yahoo.com>
2020-03-07 13:53:27 -0500

    Configure Spring MVC Controller with a web endpoint

--- comment-store/src/main/java/com/mycompany/commentstore/CommentStoreApp.java
+++ comment-store/src/main/java/com/mycompany/commentstore/CommentStoreApp.java
@@ -2,11 +2,19 @@ package com.mycompany.commentstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CommentStoreApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CommentStoreApp.class, args);
+	}
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
 	}
 }
