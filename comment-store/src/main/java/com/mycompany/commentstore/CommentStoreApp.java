e68c403888ed29134b5982c315fc62f9627c1311
vjpal3 <vrishalipal@yahoo.com>
2020-03-07 13:41:21 -0500

    Create a Maven multi-module SpringBoot project

---
+++ comment-store/src/main/java/com/mycompany/commentstore/CommentStoreApp.java
@@ -0,0 +1,12 @@
+package com.mycompany.commentstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class CommentStoreApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentStoreApp.class, args);
+	}
+}
