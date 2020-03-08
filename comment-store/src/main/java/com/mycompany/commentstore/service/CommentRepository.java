379a75e4ff2a350eb51faf5c46a569060b0216f7
vjpal3 <vrishalipal@yahoo.com>
2020-03-08 16:49:06 -0400

    Import packages for Spring data Page and Pageable

--- comment-store/src/main/java/com/mycompany/commentstore/service/CommentRepository.java
+++ comment-store/src/main/java/com/mycompany/commentstore/service/CommentRepository.java
@@ -2,6 +2,8 @@ package com.mycompany.commentstore.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.mycompany.model.Comment;
