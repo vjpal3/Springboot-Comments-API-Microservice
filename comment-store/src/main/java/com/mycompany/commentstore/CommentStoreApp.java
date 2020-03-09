b778d1b21dbad07a181835f5fd9e84dbe47c703c
vjpal3 <vrishalipal@yahoo.com>
2020-03-09 13:27:25 -0400

    Create/Update new entry in the database with Spring Transaction Management

--- comment-store/src/main/java/com/mycompany/commentstore/CommentStoreApp.java
+++ comment-store/src/main/java/com/mycompany/commentstore/CommentStoreApp.java
@@ -4,18 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+
 @ComponentScan(
 		basePackages = {
 				"com.mycompany",
 				"com.mycompany.commentsapi"
 		}
 )
+
 @EntityScan(basePackages= { "com.mycompany" })
+@EnableTransactionManagement
 public class CommentStoreApp {
 
 	public static void main(String[] args) {
--- comment-store/src/main/java/com/mycompany/commentstore/service/CommentServiceImpl.java
+++ comment-store/src/main/java/com/mycompany/commentstore/service/CommentServiceImpl.java
@@ -1,15 +1,72 @@
 package com.mycompany.commentstore.service;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.mycompany.commentsapi.spamdetection.SpamDetector;
+import com.mycompany.model.Comment;
 
 @Service
-public class CommentServiceImpl {
+public class CommentServiceImpl implements CommentService{
 
 	@Autowired 
 	private SpamDetector spamDetector;
 	
 	@Autowired
 	private CommentRepository repository;
+
+	@Override
+	@Transactional
+	public String put(Comment model) throws IOException {
+
+		if(StringUtils.isEmpty(model.getId())) {
+			model.setId(UUID.randomUUID().toString());
+		}
+		if(spamDetector.containsSpam(model.getUsernme()) || spamDetector.containsSpam(model.getEmailAddress()) 
+				|| spamDetector.containsSpam(model.getComment())) {
+			model.setSpam(true);
+		}
+		
+		final Comment dbModel = get(model.getId());
+		if(dbModel != null) {
+			dbModel.setUsernme(model.getUsernme());
+			dbModel.setComment(model.getComment());
+			dbModel.setLastModificationDate(Instant.now());
+			repository.save(dbModel);
+		}
+		else {
+			model.setCreationDate(Instant.now());
+			model.setLastModificationDate(Instant.now());
+			repository.save(dbModel);
+		}
+		return model.getId();
+	}
+
+	@Override
+	public List<Comment> list(String pageId) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	} 
+
+	@Override
+	public Comment get(String id) {
+		return repository.findById(id).orElse(null);
+	}
+
+	@Override
+	public List<Comment> listSpamComments(String pageId) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+		
+	}
 }
